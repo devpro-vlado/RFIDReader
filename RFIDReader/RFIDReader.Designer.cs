ebb00635db1bfb482a713e4c787049e8f7374d10
vlado.husko <vlado@kaloncreative.com>
2023-08-22 11:41:51 +0200

    fixes

---
+++ RFIDReader/RFIDReader.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace RFIDReader
+{
+    partial class RFIDReader
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Id = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Sucess = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Error = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Id,
+            this.Sucess,
+            this.Error});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(29, 46);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(811, 469);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // Id
+            // 
+            this.Id.Text = "Chip ID";
+            this.Id.Width = 166;
+            // 
+            // Sucess
+            // 
+            this.Sucess.Text = "Sucess";
+            this.Sucess.Width = 90;
+            // 
+            // Error
+            // 
+            this.Error.Text = "Error Message";
+            this.Error.Width = 550;
+            // 
+            // RFIDReader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(875, 539);
+            this.Controls.Add(this.listView1);
+            this.Name = "RFIDReader";
+            this.Text = "RFID Reader";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Id;
+        private System.Windows.Forms.ColumnHeader Sucess;
+        private System.Windows.Forms.ColumnHeader Error;
+    }
+}
+
---
+++ RFIDReader/RFIDReader.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Linearstar.Windows.RawInput;
+using Linearstar.Windows.RawInput.Native;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace RFIDReader
+{
+
+    public partial class RFIDReader : Form
+    {
+        private string ChipId { get; set; }
+        private HttpClient httpClient { get; set; }
+
+        public RFIDReader()
+        {
+            InitializeComponent();
+            //var devices = RawInputDevice.GetDevices();
+
+            // Keyboards will be returned as RawInputKeyboard.
+            //var keyboards = devices.OfType<RawInputKeyboard>();
+
+            RawInputDevice.RegisterDevice(HidUsageAndPage.Keyboard, RawInputDeviceFlags.InputSink, this.Handle);
+
+            httpClient = new HttpClient();
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            const int WM_INPUT = 0x00FF;
+
+            if (m.Msg == WM_INPUT)
+            {
+                var data = RawInputData.FromHandle(m.LParam);
+
+                //var sourceDeviceHandle = data.Header.DeviceHandle;
+                var sourceDevice = data.Device;
+
+                if (sourceDevice.ProductName == "RFID Reader")
+                {
+
+                    if (data is RawInputKeyboardData keyboardData && keyboardData.Keyboard.Flags == RawKeyboardFlags.None)
+                    {
+
+                        var key = (Keys)keyboardData.Keyboard.VirutalKey;
+                        if (key == Keys.Enter)
+                        {
+                            var request = new
+                            {
+                                token = "9c3f1e971b5b4f3ae5feea311ef443e5",
+                                rfid = ChipId,
+                                missed_checkpoints = 0
+                            };
+
+                            File.AppendAllText("log.txt", ChipId + Environment.NewLine);
+
+                            var result = httpClient.PostAsync("https://prijave.sdmalaerpenja.hr/api/v2/rc/1/result", 
+                                new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json")).Result;
+
+                            var isSuccess = false;
+                            var errorMessage = "";
+
+                            if (result.StatusCode == HttpStatusCode.OK)
+                            {
+                                var content = result.Content.ReadAsStringAsync().Result;
+
+                                try
+                                {
+                                    dynamic obj = JObject.Parse(content);
+                                    var resultProp = obj.result;
+                                    if (resultProp != null)
+                                    {
+                                        isSuccess = true;
+                                    }
+                                    else
+                                    {
+                                        errorMessage = $"Natjecatelj {ChipId} nije pronađen";
+                                    }
+                                }
+                                catch (Exception)
+                                {
+
+                                    var arr = JArray.Parse(content);
+                                    var resultProp = arr[1];
+                                    //if (resultProp != null)
+                                    //{
+                                    //    isSuccess = true;
+                                    //}
+                                    //else
+                                    //{
+                                    errorMessage = resultProp.ToString();
+                                    //}
+                                }
+                            
+                            }
+                            else if (result.StatusCode == HttpStatusCode.BadRequest)
+                            {
+                                var content = result.Content.ReadAsStringAsync().Result;
+                                dynamic obj = JObject.Parse(content);
+                                errorMessage = obj.error;
+                            }
+
+                            var item = new ListViewItem(ChipId);
+                            item.SubItems.Add(isSuccess ? "Yes" : "No");
+                            item.SubItems.Add(errorMessage);
+                            listView1.Items.Add(item);
+
+                            if (!isSuccess)
+                            {
+                                item.ForeColor = Color.Red;
+                            }
+
+                            ChipId = string.Empty;
+                        }
+                        else
+                        {
+                            var buf = new StringBuilder(256);
+                            ToUnicode((uint)keyboardData.Keyboard.VirutalKey, 0, new byte[256], buf, 256, 0);
+                            ChipId += buf.ToString();
+                        }
+                    }
+                }
+
+            }
+
+            base.WndProc(ref m);
+        }
+
+        [DllImport("user32.dll")]
+        public static extern int ToUnicode(uint virtualKeyCode, uint scanCode,
+            byte[] keyboardState,
+            [Out, MarshalAs(UnmanagedType.LPWStr, SizeConst = 64)]
+            StringBuilder receivingBuffer,
+            int bufferSize, uint flags);
+
+    }
+}
